4df0ef3e0ae4cc4960561639efae0e577a0376af
Giuliana2106 <62736506+Giuliana2106@users.noreply.github.com>
2020-07-02 22:07:32 -0600

    Excepciones Terminado

---
+++ Operaciones/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0;
+            char menu = 'n';
+            double n1 = 0;
+            double n2 = 0;
+            double total = 0;
+            string linea = "";
+
+
+            while (menu != 's')
+
+            {
+                Console.WriteLine("1.-suma");
+                Console.WriteLine("2.-resta");
+                Console.WriteLine("3.-multiplicación");
+                Console.WriteLine("4.-división");
+                Console.WriteLine("5.-salir");
+                Console.WriteLine("opción: ");
+
+
+                linea = Console.ReadLine();
+                op = int.Parse(linea);
+
+                try
+                {
+                    Console.WriteLine("ingrese el primer valor: ");
+                    linea = Console.ReadLine();
+                    n1 = int.Parse(linea);
+                }
+
+                catch
+                {
+                    Console.WriteLine("Dato no válido");
+                    Console.Write("Ingrese de nuevo el valor: ");
+                    linea = Console.ReadLine();
+                    n1 = int.Parse(linea);
+                }
+
+                try
+                {
+                    Console.WriteLine("ingrese el segundo valor: ");
+                    linea = Console.ReadLine();
+                    n2 = int.Parse(linea);
+                }
+
+                catch
+                {
+                    Console.WriteLine("Dato no válido");
+                    Console.Write("Ingrese de nuevo el valor: ");
+                    linea = Console.ReadLine();
+                    n2 = int.Parse(linea);
+                }
+
+                if (op == 1)
+                {
+
+                    total = n1 + n2;
+                    Console.WriteLine("El resultado es: ");
+                }
+                else if (op == 2)
+                {
+                    total = n1 - n2;
+                    Console.WriteLine("El resultado es: ");
+                }
+                else if (op == 3)
+                {
+                    total = n1 * n2;
+                    Console.WriteLine("El resultado es: ");
+                }
+                else if (op == 4)
+                {
+                    total = n1 / n2;
+                    Console.WriteLine("El resultado es: ");
+                }
+                else if (op == 5)
+                {
+                    Console.WriteLine("desea salir [s/n] ");
+                    linea = Console.ReadLine();
+                    menu = char.Parse(linea);
+                }
+
+
+            }
+        }
+    }
+}
+    
